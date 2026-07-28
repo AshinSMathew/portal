e2c04cd8c59d3ace2892ac2ca845c194321599b5
AshinSMathew <ashinmathew1010@gmail.com>
2026-07-28 16:28:31 +0530

    feat: login page mobile responsive

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -129,7 +129,28 @@ export default function LoginPage() {
       <div aria-hidden className="pointer-events-none absolute inset-0 z-0 overflow-hidden">
         <div className="absolute inset-0 bg-[#161616]" />
       </div>
-      <div aria-hidden className="pointer-events-none absolute -top-52 -right-30 z-10 h-300 w-225 overflow-visible rotate-15 opacity-90"
+
+      <div
+        className="absolute -top-40 -right-32 z-10 w-screen h-screen rotate-18 pointer-events-none sm:hidden"
+        style={{
+          background: `
+            linear-gradient(
+              225deg,
+              rgba(255,255,255,0.95) 0%,
+              rgba(210,245,255,0.92) 8%,
+              rgba(120,225,255,0.82) 18%,
+              rgba(70,185,255,0.68) 35%,
+              rgba(30,125,220,0.42) 58%,
+              rgba(15,70,180,0.18) 75%,
+              transparent 100%
+            )
+          `,
+          filter: "blur(110px)",
+          mixBlendMode: "screen",
+        }}
+      />
+
+      <div aria-hidden className="pointer-events-none absolute -top-52 -right-30 z-10 hidden h-300 w-225 overflow-visible rotate-15 opacity-80 sm:block"
         style={{
           mixBlendMode: "screen",
           filter: "blur(20px)",
@@ -144,14 +165,24 @@ export default function LoginPage() {
         />
       </div>
 
-      <div className="pointer-events-none absolute right-[9%] top-[12%] z-0 flex w-65 items-center justify-center opacity-95 sm:w-75 md:w-85 lg:w-95">
+      {/* <div className="pointer-events-none absolute right-4 top-4 z-0 block h-[180px] w-[180px] overflow-hidden rounded-full opacity-90 sm:hidden">
+        <Image
+          src="/login/telegram.png"
+          alt="telegram"
+          fill
+          priority
+          className="h-full w-full object-contain rotate-[-5deg]"
+        />
+      </div> */}
+
+      <div className="pointer-events-none absolute right-[10%] top-[10%] z-0 hidden w-65 items-center justify-center opacity-100 sm:flex sm:w-75 md:w-85 lg:w-95">
         <Image
           src="/login/telegram.png"
           alt="telegram"
           width={320}
           height={320}
           priority
-          className="h-auto w-full rotate-[-10deg] drop-shadow-[0_18px_40px_rgba(0,0,0,0.65)]"
+          className="h-auto w-full rotate-[-5deg]"
         />
       </div>
 
